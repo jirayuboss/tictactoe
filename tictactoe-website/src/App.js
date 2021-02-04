705214b0284e18fef610c39f089025684db35813
jirayuboss <59011599@kmitl.ac.th>
2021-02-05 01:21:38 +0700

    Added position for each card

--- tictactoe-website/src/App.js
+++ tictactoe-website/src/App.js
@@ -7,7 +7,15 @@ class App extends Component {
     super(props);
     this.state = {
       pic: '',
-      pos1: false
+      pos1: false,
+      pos2: false,
+      pos3: false,
+      pos4: false,
+      pos5: false,
+      pos6: false,
+      pos7: false,
+      pos8: false,
+      pos9: false,
     };
   }
 
@@ -35,118 +43,158 @@ class App extends Component {
                 </Card>
               </Col>
             </Button>
-            <Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos2: true
+              })
+            }}>
               <Col xs={6} md={4}>
                 <Card
                   bg="light"
                   text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
+                  style={{ width: 300, height: 300, alignItems: "center" }}
                   className="mb-2"
                 >
                   <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
+                    { this.state.pos2 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
                   </Card.Body>
                 </Card>
               </Col>
             </Button>
-            <Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos3: true
+              })
+            }}>
               <Col xs={6} md={4}>
                 <Card
                   bg="light"
                   text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
+                  style={{ width: 300, height: 300, alignItems: "center" }}
                   className="mb-2"
                 >
                   <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Button>
-          </Row>
-          <Row>
-            <Button>
-              <Col xs={6} md={4}>
-                <Card
-                  bg="light"
-                  text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
-                  className="mb-2"
-                >
-                  <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Button>
-            <Button>
-              <Col xs={6} md={4}>
-                <Card
-                  bg="light"
-                  text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
-                  className="mb-2"
-                >
-                  <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Button>
-            <Button>
-              <Col xs={6} md={4}>
-                <Card
-                  bg="light"
-                  text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
-                  className="mb-2"
-                >
-                  <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
+                    { this.state.pos3 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
                   </Card.Body>
                 </Card>
               </Col>
             </Button>
           </Row>
           <Row>
-            <Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos4: true
+              })
+            }}>
               <Col xs={6} md={4}>
                 <Card
                   bg="light"
                   text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
+                  style={{ width: 300, height: 300, alignItems: "center" }}
                   className="mb-2"
                 >
                   <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
+                    { this.state.pos4 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
                   </Card.Body>
                 </Card>
               </Col>
             </Button>
-            <Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos5: true
+              })
+            }}>
               <Col xs={6} md={4}>
                 <Card
                   bg="light"
                   text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
+                  style={{ width: 300, height: 300, alignItems: "center" }}
                   className="mb-2"
                 >
                   <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
+                    { this.state.pos5 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
                   </Card.Body>
                 </Card>
               </Col>
             </Button>
-            <Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos6: true
+              })
+            }}>
               <Col xs={6} md={4}>
                 <Card
                   bg="light"
                   text="light"
-                  style={{ width: 300, height: 300, textAlign: "center" }}
+                  style={{ width: 300, height: 300, alignItems: "center" }}
                   className="mb-2"
                 >
                   <Card.Body>
-                    <Image src={window.location.origin + '/playO.png'} thumbnail />
+                    { this.state.pos6 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Button>
+          </Row>
+          <Row>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos7: true
+              })
+            }}>
+              <Col xs={6} md={4}>
+                <Card
+                  bg="light"
+                  text="light"
+                  style={{ width: 300, height: 300, alignItems: "center" }}
+                  className="mb-2"
+                >
+                  <Card.Body>
+                    { this.state.pos7 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos8: true
+              })
+            }}>
+              <Col xs={6} md={4}>
+                <Card
+                  bg="light"
+                  text="light"
+                  style={{ width: 300, height: 300, alignItems: "center" }}
+                  className="mb-2"
+                >
+                  <Card.Body>
+                    { this.state.pos8 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Button>
+            <Button onClick={() => {
+              this.setState({
+                pic: '/playO.png',
+                pos9: true
+              })
+            }}>
+              <Col xs={6} md={4}>
+                <Card
+                  bg="light"
+                  text="light"
+                  style={{ width: 300, height: 300, alignItems: "center" }}
+                  className="mb-2"
+                >
+                  <Card.Body>
+                    { this.state.pos9 && <Card.Img src={window.location.origin + this.state.pic} alt="Card image" /> }
                   </Card.Body>
                 </Card>
               </Col>
